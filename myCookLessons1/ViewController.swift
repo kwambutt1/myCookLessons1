99704e770fffb6614e674e4eaeddcf2b21160714
kwambutt1 <kwambutt@googlemail.com>
2022-11-27 18:44:06 +0100

    Initial Commit

---
+++ myCookLessons1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myCookLessons1
+//
+//  Created by coder on 27.11.22.
+//  Copyright © 2022 coder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
